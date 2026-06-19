09c0ab41d43227f372f45b3305d8fa5fce366ce8
2111130134 <2806772496@qq.com>
2026-06-19 15:54:42 +0800

    Update qgc_source_zh_CN.ts

--- translations/qgc_source_zh_CN.ts
+++ translations/qgc_source_zh_CN.ts
@@ -10564,7 +10564,7 @@ Do you wish to proceed?</translation>
     <message>
       <location filename="../src/FlightDisplay/MultiRotorChecklist.qml" line="29"/>
       <source>Props mounted and secured?</source>
-      <translation>螺旋桨已安装和安全?</translation>
+      <translation>请确认螺旋桨是否安装正确</translation>
     </message>
     <message>
       <location filename="../src/FlightDisplay/MultiRotorChecklist.qml" line="50"/>
@@ -13455,7 +13455,7 @@ Click &apos;Ok&apos; to upload the Plan anyway.</translation>
     <message>
       <location filename="../src/FlightDisplay/PreFlightBatteryCheck.qml" line="19"/>
       <source>Battery connector firmly plugged?</source>
-      <translation>电池连接器是否接插牢固？</translation>
+      <translation>请确认电池电压是否正常</translation>
     </message>
     <message>
       <location filename="../src/FlightDisplay/PreFlightBatteryCheck.qml" line="22"/>
@@ -13515,7 +13515,7 @@ Click &apos;Ok&apos; to upload the Plan anyway.</translation>
     <message>
       <location filename="../src/FlightDisplay/PreFlightGPSCheck.qml" line="20"/>
       <source>Waiting for 3D lock.</source>
-      <translation>等待3D定位中。</translation>
+      <translation>3D定位中........</translation>
     </message>
     <message>
       <location filename="../src/FlightDisplay/PreFlightGPSCheck.qml" line="31"/>
@@ -13538,7 +13538,7 @@ Click &apos;Ok&apos; to upload the Plan anyway.</translation>
     <message>
       <location filename="../src/FlightDisplay/PreFlightRCCheck.qml" line="18"/>
       <source>Receiving signal. Perform range test &amp; confirm.</source>
-      <translation>接收信号中。执行拉距测试并确认。</translation>
+      <translation>请确认遥控器工作是否正常</translation>
     </message>
     <message>
       <location filename="../src/FlightDisplay/PreFlightRCCheck.qml" line="19"/>
